8af853f5e705526eca3e7b11b117d6d696b52be7
phichinad <phichinad@gmail.com>
2026-09-01 11:30:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded',function () {
   document.getElementById('year').innerHTML = new Date().getFullYear();
   console.log('pirtfolio ของ phichinad พร้อมแล้ว !');
+
 });
